1ab8a65d274c31f6e787950262cfccfb0719fe39
Anupam Chaudhary <chaudharyanupam746@gmail.com>
2026-03-06 15:56:53 +0530

    Refactor database URL handling in config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import pymysql
 import os
 class Config:
+    db_url = os.environ.get("DATABASE_URL")
+
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
     SECRET_KEY = "secret-key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
