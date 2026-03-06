b5f2e983b06f6f72f9c1f3b06570d1cf4fd190a5
Anupam Chaudhary <chaudharyanupam746@gmail.com>
2026-03-06 15:12:51 +0530

    Use environment variable for database URI

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import pymysql
-
+import os
 class Config:
     SECRET_KEY = "secret-key"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://sarthak:sarthak114942@localhost:3306/to_do_app"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
